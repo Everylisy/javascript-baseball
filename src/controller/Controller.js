31fff999edb54c9a9948168a66ffae6888bd09a9
violetwhenisawu <anashape710@gmail.com>
2022-12-16 23:51:48 +0900

    feat: 경주 할 자동차 이름 입력, 유효성 검사 기능 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,5 +1,29 @@
+const InputView = require('../view/inputView');
+const validator = require('../utils/InputValidator');
+
 class Controller {
-  start() {}
+  #carNamesArr;
+
+  constructor() {
+    this.#carNamesArr;
+  }
+
+  start() {
+    this.inputCarName();
+  }
+
+  inputCarName() {
+    InputView.readCarName(this.getCarName.bind(this));
+  }
+
+  getCarName(name) {
+    try {
+      this.#carNamesArr = name.split(',');
+      validator.checkCarName(this.#carNamesArr);
+    } catch {
+      this.inputCarName();
+    }
+  }
 }
 
 module.exports = Controller;
--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -1,3 +1,17 @@
-const validator = {};
+const { Console } = require('@woowacourse/mission-utils');
+const validator = {
+  checkCarName(input) {
+    if (this.checkNameLength(input)) return;
+    throw Console.print('[ERROR] 입력한 자동차 이름이 유효하지 않습니다.');
+  },
+
+  checkNameLength(names) {
+    function checkLength(name) {
+      return 0 < name.length && name.length < 5;
+    }
+
+    return names.every(checkLength);
+  },
+};
 
 module.exports = validator;
